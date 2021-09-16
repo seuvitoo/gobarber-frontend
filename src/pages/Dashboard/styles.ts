bd162eab9cb82bf1c004318fe3153ca0ebce078c
Victor Santos <vy.santos@live.com>
2021-09-16 19:02:09 -0300

    [FEATURE] adicionando perfil o usuarios

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -41,8 +41,14 @@ export const Profile = styled.div`
     span {
       color: #f4ede8;
     }
-    strong {
+    a {
       color: #ff9000;
+      text-decoration: none;
+
+      &:hover {
+        opacity: 0.8;
+      }
+
     }
   }
 `;
